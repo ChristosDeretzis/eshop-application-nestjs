9a7e66a4e8753b15e8dff5e80533aa4c8603274f
Christos Deretzis <deretzischristos@gmail.com>
2021-05-04 23:20:22 +0300

    Initial commit

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { Mongoose } from 'mongoose';
+import { UserSchema } from 'src/user/schemas/user.schema';
+import { AuthService } from './auth.service';
+import { RefreshTokenSchema } from './schemas/refresh-token.schema';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'User', schema: UserSchema },
+      { name: 'RefreshToken', schema: RefreshTokenSchema },
+    ]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRATION },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService]
+})
+export class AuthModule {}
---
+++ src/user/interfaces/user.interface.ts
@@ -0,0 +1,17 @@
+import { Document } from "mongoose";
+
+export interface User extends Document {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    username: string;
+    roles: [string];
+    verification: string;
+    verified: boolean;
+    verificationExpires: boolean;
+    loginAttempts?: number;
+    blockExpires?: Date;
+    bankAccountNumber?: string;
+    bankAccountName?: string;
+}
