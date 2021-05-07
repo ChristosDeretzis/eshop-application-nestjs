facb0a098d89dcc815425cd796825edef0010ff9
Christos Deretzis <deretzischristos@gmail.com>
2021-05-07 23:57:00 +0300

    Add login and email verification routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { RolesGuard } from 'src/auth/guard/roles.guard';
 import { CreateUserDTO } from './dtos/create-user.dto';
+import { VerifyUuidDto } from './dtos/verify-uuid.dto';
 import { UserService } from './user.service';
+import { Request } from 'express';
+import { LoginUserDTO } from './dtos/login-user.dto';
 
 @ApiTags('User')
 @Controller('user')
@@ -19,5 +22,21 @@ export class UserController {
     @ApiCreatedResponse({})
     async register(@Body() createUserDTO: CreateUserDTO) {
         return this.userService.create(createUserDTO);
+    }
+
+    @Post('verify-email')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({summary: 'Verify Email',})
+    @ApiOkResponse({})
+    async verifyEmail(@Req() req: Request, @Body() verifyUuidDto: VerifyUuidDto) {
+        return await this.userService.verifyEmail(req, verifyUuidDto);
+    }
+
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({summary: 'Login User',})
+    @ApiOkResponse({})
+    async login(@Req() req: Request, @Body() loginUserDTO: LoginUserDTO) {
+        return await this.userService.login(req, loginUserDTO);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { AuthService } from 'src/auth/auth.service';
@@ -7,6 +7,10 @@ import { ForgotPassword } from './interfaces/forgot-password.interface';
 import { User } from './interfaces/user.interface';
 import { v4 } from 'uuid';
 import { addHours } from 'date-fns';
+import { Request } from 'express';
+import { VerifyUuidDto } from './dtos/verify-uuid.dto';
+import { LoginUserDTO } from './dtos/login-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -30,13 +34,44 @@ export class UserService {
         return this.buildRegistrationInfo(user);
     }
 
+    async verifyEmail(req: Request, verifyUuidDto: VerifyUuidDto) {
+        const user = await this.findByVerification(verifyUuidDto.verification);
+        await this.setUserAsVerified(user);
+        return {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            username: user.username,
+            email: user.email,
+            accessToken: await this.authService.createAccessToken(user._id),
+            refreshToken: await this.authService.createRefreshToken(req, user._id),
+        };
+    }
+
+    async login(req: Request, loginUserDTO: LoginUserDTO) {
+        const user = await this.findUserByEmail(loginUserDTO.email);
+        this.isUserBlocked(user);
+        await this.checkPassword(loginUserDTO.password, user);
+        await this.passwordsAreMatch(user);
+        return {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            usermame: user.username,
+            email: user.email,
+            accessToken: await this.authService.createAccessToken(user._id),
+            refreshToken: await this.authService.createRefreshToken(req, user._id),
+        }
+    }
+
+
+    // Helper functions
+
     private setRegistrationInfo(user: User): any {
         user.verification = v4();
         user.verificationExpires = addHours(new Date(), this.HOURS_TO_VERIFY);
     }
 
     private async isEmailUnique(email: string) {
-        const user = this.userModel.findOne({email, verified: true});
+        const user = await this.userModel.findOne({email, verified: true});
         if(user){
             throw new BadRequestException('Email must be unique');
         }
@@ -44,10 +79,68 @@ export class UserService {
 
     private buildRegistrationInfo(user): any {
         const userRegistrationInfo = {
-            fullName: user.fullName,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            username: user.username,
             email: user.email,
             verified: user.verified,
         };
         return userRegistrationInfo;
     }
+
+    private async findByVerification(verification: string): Promise<User> {
+        const user = await this.userModel.findOne({verification, verified: false, verificationExpires: {$gt: new Date()}});
+        if (!user) {
+            throw new BadRequestException('Bad request.');
+        }
+        return user;
+    }
+
+    private async setUserAsVerified(user) {
+        user.verified = true;
+        await user.save();
+    }
+
+    private async findUserByEmail(email: string): Promise<User> {
+        const user = await this.userModel.findOne({email, verified: true});
+        if (!user) {
+          throw new NotFoundException('Wrong email or password.');
+        }
+        return user;
+    }
+
+    private isUserBlocked(user) {
+        if (user.blockExpires > Date.now()) {
+            throw new ConflictException('User has been blocked try later.');
+        }
+    }
+
+    private async checkPassword(attemptPassword: string, user) {
+        const match = await bcrypt.compare(attemptPassword, user.password);
+        if (!match) {
+            await this.passwordsDoNotMatch(user);
+            throw new NotFoundException('Wrong email or password.');
+        }
+        return match;
+    }
+
+    private async passwordsDoNotMatch(user) {
+        user.loginAttempts += 1;
+        await user.save();
+        if (user.loginAttempts >= this.LOGIN_ATTEMPTS_TO_BLOCK) {
+            await this.blockUser(user);
+            throw new ConflictException('User blocked.');
+        }
+    }
+
+    private async blockUser(user) {
+        user.blockExpires = addHours(new Date(), this.HOURS_TO_BLOCK);
+        await user.save();
+    }
+
+    private async passwordsAreMatch(user) {
+        user.loginAttempts = 0 ;
+        await user.save();
+    }
+
 }
